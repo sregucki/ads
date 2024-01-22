d2d287528d36ee25d675b18b79136d4bedd34607
sregucki <sebastian.regucki@outlook.com>
2024-01-22 15:49:53 +0100

    [LinkedList][4/11] Fix ReorderList

--- src/main/java/neetcode/linkedlist/RemoveNthNodeFromEndOfList.java
+++ src/main/java/neetcode/linkedlist/RemoveNthNodeFromEndOfList.java
@@ -38,15 +38,16 @@ public class RemoveNthNodeFromEndOfList {
 		for (int i = 0; i < n; i++) {
 			fast = fast.next;
 		}
+		if (fast == null) {
+			return head.next;
+		}
 		while (fast.next != null) {
 			fast = fast.next;
 			slow = slow.next;
 		}
 		slow.next = slow.next.next;
-		System.out.println(fast);
-		System.out.println(slow);
 
-		return null;
+		return head;
 	}
 
 	public static void main(String[] args) {
