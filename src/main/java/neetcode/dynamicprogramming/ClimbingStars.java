9ed5e00a9015c12e566696227a6d19f1b69ef700
sregucki <sebastian.regucki@outlook.com>
2024-03-28 19:28:10 +0100

    ClimbingStars refinement

--- src/main/java/neetcode/dynamicprogramming/ClimbingStars.java
+++ src/main/java/neetcode/dynamicprogramming/ClimbingStars.java
@@ -4,17 +4,17 @@ public class ClimbingStars {
 
 	/*
 	Time complexity: O(n)
-	Space complexity: O(1)
+	Space complexity: O(n)
 	 */
 	public int climbStairs(int n) {
 		// bottom-up approach
-		final int[] dp = new int[n + 1];
-		dp[n] = 1;
-		dp[n - 1] = 1;
-		for (int i = n - 2; i >= 0; i--) {
-			dp[i] = dp[i + 1] + dp[i + 2];
+		final int[] arr = new int[n + 1];
+		arr[0] = 1;
+		arr[1] = 1;
+		for (int i = 2; i < n + 1; i++) {
+			arr[i] = arr[i - 1] + arr[i - 2];
 		}
-		return dp[0];
+		return arr[n];
 	}
 
 	public static void main(String[] args) {
